f84cf7a6dda44dc4c10cbb4723fb4ce7d076ecbc
Иван <aminogoule@gmail.com>
2024-10-11 17:58:54 +0300

    start with Unity and C#

---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    //В этом файле будут происходить действия из глав 1 2 3 4
+    void Start()
+    {
+        Debug.Log("starting to learn C#");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
