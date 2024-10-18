cea5c8b291d6c347e254f4b9ea763fce9df6f5d0
Иван <aminogoule@gmail.com>
2024-10-18 21:26:46 +0300

    add phys amd fix

--- Assets/Scripts/Camera_Behavior.cs
+++ Assets/Scripts/Camera_Behavior.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Camera_Behavior : MonoBehaviour
 {
-    private Rigidbody _rb;
+    
     public Vector3 camOffset = new(0f,1.3f,-2.6f);
     [SerializeField]private Transform target;
     public void Start()
     {
         target = GameObject.Find("Player").transform;
         Debug.Log("Camera script active");
-        _rb = GetComponent<Rigidbody>();
+        
 
     }
     public void LateUpdate() //не путать с LastUpdate
--- Assets/Scripts/Player_Behavior.cs
+++ Assets/Scripts/Player_Behavior.cs
@@ -8,10 +8,16 @@ public class Player_Behavior : MonoBehaviour
     public float rotationSpeed = 75f;
     private float vInput;
     private float hInput;
+    private Rigidbody _rb;
     // Start is called before the first frame update
     // This is empty, and because it removed
 
     // Update is called once per frame
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         //Одновременно работать либо с RigidBody либо прямое управвление через трансформацию
@@ -21,5 +27,13 @@ public class Player_Behavior : MonoBehaviour
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
         */
+    }
+    void FixedUpdate()
+    {
+        Vector3 rotatation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotatation * Time.fixedDeltaTime);
+        //необходимо пояснение по этой функции 
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
