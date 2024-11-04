9b2085440644429839792e5c52158ff9ffcd7578
Иван <aminogoule@gmail.com>
2024-11-04 23:28:52 +0300

    add_assets

---
+++ Assets/Low Poly Fruits/resources/fruits_tester.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fruits_tester : MonoBehaviour {
+
+	public GameObject[] fruits;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//delete rotate
+	
+	}
+}
